c92fd17b10612b3998d0cd034bd8d7afe2e0fdeb
pranay316 <166217282+pranay316@users.noreply.github.com>
2024-06-16 04:56:02 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,11 @@
+const gameName = "pranay"
+const newName = gameName;
+console.log(newName);
+
+const newString = new String("Mahesh")
+console.log(newString);
+
+const s1 = "2 + 2"
+const s2 = new String("2 + 2")
+console.log(eval(s1));
+console.log(eval(s2));
